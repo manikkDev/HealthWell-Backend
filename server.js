819445abff25320a7d4e6fe4f42a7981b1c8f3e6
Manik <beingdeveloper84@gmail.com>
2025-08-30 19:32:46 +0530

    commit for backend

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Configure CORS to allow requests from your frontend's origin
+app.use(cors({
+  origin: 'https://healthwell-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // Connect to MongoDB and then start the server
 connectToDatabase()
   .then(() => {
